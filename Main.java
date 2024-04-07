c2ca9150c63b6e045dc5af04623e7721d8f66e7a
Adam Ostoja-Helczynski <ahelczynski@gmail.com>
2024-04-07 12:58:56 +0200

    zad4

--- Main.java
+++ Main.java
@@ -3,14 +3,18 @@ import geometry.Point;
 import geometry.ColoredCircle;
 
 public class Main {
+
     public static void main(String[] args) {
-        Point center = new Point(1.0, 2.0);
-        ColoredCircle circle = new ColoredCircle(center, 5.0, "Czerwony");
-        System.out.println("Koło: " + circle);
-        System.out.println("Kolor koła: " + circle.getColor());
-        System.out.println("Środek koła: " + circle.getCenter());
-        System.out.println("Promień koła: " + circle.getRadius());
-        System.out.println("Obwód koła: " + circle.getCircumference());
-        System.out.println("Pole powierzchni koła: " + circle.getArea());
+        Circle[] circles = new Circle[3];
+        circles[0] = new Circle(new Point(1.0, 2.0), 5.0);
+        circles[1] = new ColoredCircle(new Point(3.0, 4.0), 6.0, "Czerwony");
+        circles[2] = new Circle(new Point(5.0, 6.0), 7.0);
+        for (Circle circle : circles) {
+            System.out.println("Pole powierzchni koła: " + circle.getArea());
+            if (circle instanceof ColoredCircle) {
+                ColoredCircle coloredCircle = (ColoredCircle) circle;
+                System.out.println("Kolor koła: " + coloredCircle.getColor());
+            }
+        }
     }
 }

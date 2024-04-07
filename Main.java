4016d93e29c0a8674cbb3cc2e1c4ad51e634e783
Adam Ostoja-Helczynski <ahelczynski@gmail.com>
2024-04-07 11:50:42 +0200

    zad1

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Point punkt = new Point(1.5, 3.0);
+
+        System.out.println("Współrzędne punktu:");
+        System.out.println("x= " + punkt.getX());
+        System.out.println("y= " + punkt.getY());
+    }
+}
---
+++ Point.java
@@ -0,0 +1,30 @@
+public class Point {
+    private double x;
+    private double y;
+      public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+    public double getX() {
+        return x;
+    }
+    public double getY() {
+        return y;
+    }
+    public void setX(double x) {
+        this.x = x;
+    }
+    public void setY(double y) {
+        this.y = y;
+    }
+    @Override
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
+       public static void main(String[] args) {
+        Point p1 = new Point(2.0, 1.0);
+        Point p2 = new Point(3.0, 6.0);
+        System.out.println("Punkt p1: " + p1);
+        System.out.println("Punkt p2: " + p2);
+    }
+}
